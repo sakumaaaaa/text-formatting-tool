54a310b1e337fdba1aba4aaec69a5c30ee77acf3
sakumaaaaa <90688118+sakumaaaaa@users.noreply.github.com>
2026-01-04 15:23:51 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const OPT_KEYS = [
+    'opt_percent','opt_ampersand','opt_bracket','opt_colon',
+    'opt_punctuation','opt_quote','opt_wave','opt_mark',
+    'opt_dash','opt_hyphen','opt_slash','opt_equal', 'opt_mark_space'
+];
+
+export const DEFAULT_USER = "sakumaaaaa";
+export const DEFAULT_REPO = "text-formatting-tool";
